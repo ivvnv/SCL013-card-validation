2309eb9424335d0f4ebe3cc7a49a66240435b065
“ivvnv” <ivana.rivera.c@gmail.com>
2020-04-14 01:05:15 -0300

    inclusion codigo formulario y img

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
 
+document.getElementById("submitBtn").addEventListener("click", function(){
+    let creditCardNumber = document.getElementById("card").value;
+    if( validator.isValid(creditCardNumber) ) {
+    document.getElementById("resultDiv").innerHTML = "Tu tarjeta " + validator.maskify(creditCardNumber) + " es válida ✓";
+    document.getElementById("resultDiv").style.color = '#51a400';
+  } 
+    else {
+    document.getElementById("resultDiv").innerHTML = validator.maskify(creditCardNumber) + " no es una tarjeta válida";
+    document.getElementById("resultDiv").style.color = '#E80460';
+      }
+    },
+    false);
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
-  // ...
+  isValid(creditCardNumber) {
+  
+  
+    var arr = []; //usamos array para almacenar los dígitos del input
+    
+    for (let i = 0; i < creditCardNumber.length; i++) { //for loop para iterar a través de cada dígito de creditCardNumber
+  
+    if (i % 2 == 0) { //en el contexto del array el index es par o impar, usando el operador módulo
+    
+    if (creditCardNumber[i]*2 < 10) { //si un dígito multiplicado por dos es menor que 10, 
+    arr.push(creditCardNumber[i]*2); //lo añadimos al array
+    
+    } else {
+    arr.push(creditCardNumber[i]*2-9); //si es mayor o igual que 10, le sustraemos 9, y lo añadimos al array
+    }
+    
+    } else { //después del if else se añaden los dígitos impares
+    arr.push(parseInt(creditCardNumber[i],10)) //con parseInt para asegurarnos de que sea un num entero
+    }
+    }
+  
+    return arr.reduce( (prv, cur) => prv + cur) % 10 === 0; //con reduce podemos sumar el array entero y comprobar con operador modulo si es multiplo de 10
+    },
+  
+  
+  
+  
+    //Para enmascarar creditCardNumber
+    maskify(creditCardNumber) {
+    var lastFour = "";
+    lastFour = creditCardNumber.slice(-4);
+    var maskedNumbers = "";
+    
+    for(var i = (creditCardNumber.length)-4; i>0; i--){
+    maskedNumbers += "#";
+    }
+    
+    creditCardNumber = maskedNumbers+lastFour;
+  
+    return creditCardNumber
+  
+  }
 };
 
 export default validator;
