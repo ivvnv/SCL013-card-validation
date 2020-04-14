c6ed52ce91918a766089d7d9a428a051fc9f7574
“ivvnv” <ivana.rivera.c@gmail.com>
2020-04-14 16:05:43 -0300

    ultimos cambios

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -27,7 +27,7 @@ describe('validator', () => {
 
     test('Debería retornar "############5616" para "4556364607935616"', () => {
       expect(validator.maskify("4556364607935616")).toBe("############5616");
-      // escribe aquí tu test
+
     });
   });
 });
